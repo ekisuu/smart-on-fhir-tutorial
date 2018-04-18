84ea51a394eadc15f05778856219597277c6e338
ekisuu <38327821+ekisuu@users.noreply.github.com>
2018-04-18 16:52:23 -0400

    Update example-smart-app.js

--- source/example-smart-app/src/js/example-smart-app.js
+++ source/example-smart-app/src/js/example-smart-app.js
@@ -69,7 +69,7 @@
 
           p.hdl = getQuantityValueAndUnit(hdl[0]);
           p.ldl = getQuantityValueAndUnit(ldl[0]);
-          p.oralTemp = oralTemp;
+          p.oralTemp = diastolicbp;
          
 
           ret.resolve(p);
