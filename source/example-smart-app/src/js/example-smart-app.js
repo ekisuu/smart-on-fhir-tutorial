4d6a156b20205f2cbb8007dc385f71a7905b10f4
ekisuu <38327821+ekisuu@users.noreply.github.com>
2018-04-18 17:14:21 -0400

    Update example-smart-app.js

--- source/example-smart-app/src/js/example-smart-app.js
+++ source/example-smart-app/src/js/example-smart-app.js
@@ -17,7 +17,7 @@
                       code: {
                         $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
                               'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
-                              'http://loinc.org|2089-1', 'http://loinc.org|55284-4','http://loinc.org|8331-1']
+                              'http://loinc.org|2089-1', 'http://loinc.org|55284-4','http://loinc.org|8310-5']
                               
                       }
                     }
@@ -48,7 +48,7 @@
           var diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
           var hdl = byCodes('2085-9');
           var ldl = byCodes('2089-1');
-          var oralTemp = byCodes('8331-1');
+          var oralTemp = byCodes('8310-5');
           
 
           var p = defaultPatient();
