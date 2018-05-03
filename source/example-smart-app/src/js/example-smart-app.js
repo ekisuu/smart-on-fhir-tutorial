f49e2d3102254aee1959757460eb92ece4108da3
ekisuu <38327821+ekisuu@users.noreply.github.com>
2018-05-03 13:29:19 -0400

    Update example-smart-app.js

--- source/example-smart-app/src/js/example-smart-app.js
+++ source/example-smart-app/src/js/example-smart-app.js
@@ -23,7 +23,7 @@
                       }
                     }
                   }
-                  {
+                  //{
                   /*type : 'Device',
                   query:{
                             model     : ''
